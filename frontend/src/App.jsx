b5d4df2e3deb53192821cdc74c83197057e1cb81
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-10-17 16:15:35 +1100

    Verify Code page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import FloatingShape from './components/FloatingShape';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
+import EmailVerification from './pages/EmailVerification';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
         <Route path='/' element={'Home'} />
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/verify-email' element={<EmailVerification />} />
       </Routes>
     </div>
   );
